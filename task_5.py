0c4bc2effd24e3bb5db2df72c2d59144226ae1ac
dpotanin44-tech <dpotanin44@gmail.com>
2026-06-25 14:25:10 +0700

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,1 +1,26 @@
+with open('resource/words.txt', 'r') as f:
+    words = [line.strip() for line in f if line.strip()]
 
+
+alphabetical = sorted(words)
+
+
+by_length = sorted(words, key=len)
+
+
+reverse = sorted(words, reverse=True)
+
+
+with open('resource/sorted_alphabetically.txt', 'w') as f:
+    for word in alphabetical:
+        f.write(word + '\n')
+
+with open('resource/sorted_by_length.txt', 'w') as f:
+    for word in by_length:
+        f.write(word + '\n')
+
+with open('resource/sorted_reverse.txt', 'w') as f:
+    for word in reverse:
+        f.write(word + '\n')
+
+print("Создано 3 файла")
